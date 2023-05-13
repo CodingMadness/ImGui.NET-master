ef11fa72a453cf0ba402954fbf678ea326b73ae7
H o t i, Shpend <shho310795@proton.me>
2023-05-13 23:49:10 +0200

    Initial commit

---
+++ ImGui.NET-master/src/ImGui.NET/ImDrawData.Manual.cs
@@ -0,0 +1,7 @@
+﻿namespace ImGuiNET
+{
+    public readonly unsafe partial struct ImDrawDataPtr
+    {
+        public RangePtrAccessor<ImDrawListPtr> CmdListsRange => new RangePtrAccessor<ImDrawListPtr>(CmdLists.ToPointer(), CmdListsCount);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ImGui.NET-master
+ Slighlty reworked Clone of the Original repo to allow ReadOnlySpan<char> to be passed in insteaf of string
